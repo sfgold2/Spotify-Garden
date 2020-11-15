dea62ec3f0555d74c2f676bdb56a86e2133c28f7
Shaun Gold <sfgold2@gmail.com>
2020-11-14 17:23:52 -0800

    function to get valence and tempo

--- src/App.js
+++ src/App.js
@@ -13,16 +13,32 @@ import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 // Website imports for classes you made
 import { Card } from "app/containers";
 
+const token = "BQCrb0lEfby8kupiV7Upl8wTB3WUChWZSmUEZWu4ZzQAV08Kl48220O0gSU8z19dvpGnOagF-LO8r4Tx5swsfJHGBDIlFIkX_f4jwJrUoHFe2UYQPvLaplo48tUSUa750JfJrTSnW1zj6xY5MA"
+
 function SongCard() {
 
-    // ** we added
+    // ** function to find 10 songs and return it as an array
     
-    const [data, setData] = useState(null);
+    const [songName, setData] = useState(null);
 
-    useEffect(()=>{
-      var token = "BQAPXC3x0Z1PWE3yYRZ9nUnG8PFv7zexMDlTCMYu3O9VvoNxZR_btztnaS7akIoFpFe5f_ZL6Z4m2HyX1szMC1U1BNti4ToXwe_hyvnkULoomtAUwEswYB5nr85Khro5wdRWZeUQcHZanSMTVO17";
+    useEffect( ()=>{
+async function fetchData(){
+let TopSongsAndfeatures = await getTopSongsAndFeatures()
+setData(TopSongsAndfeatures)}
+fetchData()
+}
 
-    axios.get("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=1&offset=1", 
+, [])
+
+  return (
+    <Card style={{ width: "30vw"}}>
+      <p> {songName!=null ? JSON.stringify(songName) : null} </p>
+    </Card>
+  );
+}
+
+async function getSongFeatures(id){
+  let response = await axios.get("https://api.spotify.com/v1/audio-features/" + id, 
       {
         headers: {
             Accept: "application/json", 
@@ -30,27 +46,40 @@ function SongCard() {
             Authorization: "Bearer " + token
         }
       }
-    ).then(function(response){
-        let map_names = response.data.items.map(x => x.id)
-        console.log(map_names)
-        setData(map_names);
-      }
-    );
-    }, [])
+    )
+  return (response.data)
 
-  return (
-    <Card style={{ width: "30vw"}}>
-      <p> {data!=null ? JSON.stringify(data) : null} </p>
-    </Card>
-  );
+}
+async function getTopSongsAndFeatures() {
+
+  // ** function to get mood and tempo for each song
+
+ let response = await axios.get("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=1&offset=1", 
+    {
+      headers: {
+          Accept: "application/json", 
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token
+      }
+    }
+  )
+  let FeatureArray = []
+  for await(let x of response.data.items){
+    let songFeatures = await getSongFeatures(x.id);
+    console.log(songFeatures)
+    FeatureArray.push({id: x.id,
+      name: x.name,
+      features: songFeatures})
+  }
+  return FeatureArray
 }
 
-function BasicFlower() {
+ function BasicFlower() {
 
-return (
-  <Card style={{ width: "30vw"}}>
-    <p> hi riley </p>
-  </Card>
+ return (
+   <Card style={{ width: "30vw"}}>
+     <p> hi riley </p>
+   </Card>
 );
 
 }
