151d837d4c4c921063ff460747809c17bc0f333b
Lee <lbahir@uci.edu>
2020-11-15 02:37:22 -0800

    cleaned, added descriptions

--- src/app/containers/flower/Flower.js
+++ src/app/containers/flower/Flower.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './Flower.scss';
 
-
 function Flower(props) {
   return (
     <div >
--- src/app/containers/index.js
+++ src/app/containers/index.js
@@ -1,3 +1,2 @@
 export { default as Card } from "../containers/card/Card";
-export { default as Garden } from "../containers/garden/Garden";
 export { default as Flower } from "../containers/flower/Flower";
